9e08aab88e50fc07c90a2c2552ad70964a676813
Pedro Santos <pedro.m.paixao.santos@gmail.com>
2016-10-05 17:09:24 +0100

    a1

--- tests/series/serie1/CountEqualsTest.java
+++ tests/series/serie1/CountEqualsTest.java
@@ -18,6 +18,7 @@ public class CountEqualsTest {
 	@Test
 	public void countEquals_OnAnArraysWithOneEqualElementInTheMiddle() {
 		int[] v1 = new int[]{1, 3, 4, 5, 7, 10, 12};
+		//int[] v1 = new int[]{ 15, 25, 35, 45, 55, 65, 75 };
 		int[] v2 = new int[]{0, 2, 6, 7, 11, 13};
 		int count = countEquals(v1, 0, 6, v2, 0, 5);
 		assertEquals(1, count);
