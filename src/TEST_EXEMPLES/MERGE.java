b673d49f2c602b46761afba2c97bd1a7f1b805a3
Pedro Santos <pedro.m.paixao.santos@gmail.com>
2016-10-10 20:43:09 +0100

    a1

--- src/TEST_EXEMPLES/MERGE.java
+++ src/TEST_EXEMPLES/MERGE.java
@@ -1,5 +1,5 @@
-package TEST_EXEMPLES;
-
+package TEST_EXEMPLES;/*
+/*
 /**
  * Created by Pedro on 08/10/2016.
  */
@@ -8,7 +8,7 @@ public class MERGE {
 		int[] array1 = {7, 22, 35, 42, 22, 35, 47, 55};
 		int[] array2 = {22, 42, 47, 42, 22, 35, 35, 35};
 
-		System.out.println(isPermutation(array1, 2, 5, array2, 3, 6));
+		//System.out.println(isPermutation(array1, 2, 5, array2, 3, 6));
 	}
 	public static int greatestAfterRotate(int[] v, int l, int r){
 		if(l == r) return -1;
@@ -19,7 +19,7 @@ public class MERGE {
 		return v[r];
 	}
 
-	public static boolean isPermutation(int[] a1, int l1, int r1, int[] a2, int l2, int r2){
+	/*public static boolean isPermutation(int[] a1, int l1, int r1, int[] a2, int l2, int r2){
 		if(l1-r1 != l2-r2) return false;
 		int count = 0;
 		for(int i = l1; i<=r1; i++) {
@@ -37,5 +37,5 @@ public class MERGE {
 			}
 		}
 		return (count == (r1-l1)+1) ? true : false;
-	}
+	}*/
 }
